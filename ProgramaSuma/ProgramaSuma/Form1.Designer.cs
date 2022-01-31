b75cd88df091576b8fcf03b0fe8d06046a391ebc
JeancaG <98427356+JeancaG@users.noreply.github.com>
2022-01-31 14:10:01 -0600

    Primer Programa
    Primer programa visto en clase

---
+++ ProgramaSuma/ProgramaSuma/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace ProgramaSuma
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Numero1textBox = new System.Windows.Forms.TextBox();
+            this.Numero2textBox = new System.Windows.Forms.TextBox();
+            this.ResultadotextBox = new System.Windows.Forms.TextBox();
+            this.Sumarbutton = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(91, 94);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Número 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(91, 153);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Número 2";
+            // 
+            // Numero1textBox
+            // 
+            this.Numero1textBox.Location = new System.Drawing.Point(208, 86);
+            this.Numero1textBox.Name = "Numero1textBox";
+            this.Numero1textBox.Size = new System.Drawing.Size(100, 23);
+            this.Numero1textBox.TabIndex = 2;
+            // 
+            // Numero2textBox
+            // 
+            this.Numero2textBox.Location = new System.Drawing.Point(208, 145);
+            this.Numero2textBox.Name = "Numero2textBox";
+            this.Numero2textBox.Size = new System.Drawing.Size(100, 23);
+            this.Numero2textBox.TabIndex = 3;
+            // 
+            // ResultadotextBox
+            // 
+            this.ResultadotextBox.Location = new System.Drawing.Point(208, 224);
+            this.ResultadotextBox.Name = "ResultadotextBox";
+            this.ResultadotextBox.Size = new System.Drawing.Size(100, 23);
+            this.ResultadotextBox.TabIndex = 4;
+            // 
+            // Sumarbutton
+            // 
+            this.Sumarbutton.Location = new System.Drawing.Point(443, 115);
+            this.Sumarbutton.Name = "Sumarbutton";
+            this.Sumarbutton.Size = new System.Drawing.Size(75, 23);
+            this.Sumarbutton.TabIndex = 5;
+            this.Sumarbutton.Text = "Sumar";
+            this.Sumarbutton.UseVisualStyleBackColor = true;
+            this.Sumarbutton.Click += new System.EventHandler(this.Sumarbutton_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(91, 232);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 15);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Resultado";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(429, 191);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(105, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Mostrar Mensaje";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Sumarbutton);
+            this.Controls.Add(this.ResultadotextBox);
+            this.Controls.Add(this.Numero2textBox);
+            this.Controls.Add(this.Numero1textBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox Numero1textBox;
+        private TextBox Numero2textBox;
+        private TextBox ResultadotextBox;
+        private Button Sumarbutton;
+        private Label label3;
+        private Button button1;
+    }
+}
---
+++ ProgramaSuma/ProgramaSuma/Form1.cs
@@ -0,0 +1,42 @@
+namespace ProgramaSuma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Sumarbutton_Click(object sender, EventArgs e)
+        {
+            int num1 = Convert.ToInt32(Numero1textBox.Text);
+            int num2 = Convert.ToInt32(Numero2textBox.Text);
+
+            Sumar(num1, num2);
+
+            ResultadotextBox.Text = Sumar(num1, num2).ToString();
+
+        }
+
+        private int Sumar(int num1, int num2)
+        {
+            int resultado = num1 + num2;
+            return resultado;
+        }
+
+        private void MostrarMensaje()
+        {
+            MessageBox.Show("Este es un mensaje");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MostrarMensaje();
+        }
+    }
+}
